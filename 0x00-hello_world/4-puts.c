ed0e3836f00a988886dc97289d41319991ceb8bc
SamawiM <c.samawimanka@gmail.com>
2022-06-02 12:23:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,26 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *
- *  * main - my entry point
- *
- *   * description -using the puts function for our code
- *
- *    * it prints out "prgrammint is like building a multilingual puzzle"
- *
- *     * Return : Always 0 (success)
- *
- *      * description : if there is no error in our code, it should return integer 0
- *
- *       */
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
