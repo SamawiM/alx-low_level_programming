7d1bff3246c79eb1eb240de3cd35e91f458098c0
SamawiM <c.samawimanka@gmail.com>
2022-06-02 05:11:05 -0700

    Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,26 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *  * main - my entry point
+ *
+ *   * description -using the puts function for our code
+ *
+ *    * it prints out "prgrammint is like building a multilingual puzzle"
+ *
+ *     * Return : Always 0 (success)
+ *
+ *      * description : if there is no error in our code, it should return integer 0
+ *
+ *       */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
